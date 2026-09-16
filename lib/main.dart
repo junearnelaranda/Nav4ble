d561fd89bc7962f83d8d1766bc21e5a4f9559861
Fernandez Larry <laryfernandez12@gmail.com>
2026-09-17 01:29:42 +0800

    Configure Firebase for Nav4ble

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
+import 'firebase_options.dart';
 import 'navable_design.dart';
 import 'welcome.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
